63e7913ef5332745276ea2afc17a94d2e329a8e9
Eetuseppala <eetu.seppala@luukku.com>
2014-03-13 20:27:35 +0200

    kolmas testi

--- Avaruuspeli/src/main/java/avaruuspeli/avaruuspeli/App.java
+++ Avaruuspeli/src/main/java/avaruuspeli/avaruuspeli/App.java
@@ -1,10 +1,10 @@
 package avaruuspeli.avaruuspeli;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+        public static void main( String[] args )
+    {
+        System.out.println( "Avaruuspeli" );
+    }
     
 }
